8f5f08142b27de7c75d9796a7f6eea2829834863
hundredvisionsguy <cwinikka@gmail.com>
2017-05-10 16:06:26 -0700

    adding .gitignore and if __name__ == __main__ section

--- answerCall.py
+++ answerCall.py
@@ -6,5 +6,9 @@
 
 # Write function defintion: answerCall()
 
-    
 # Make sure it returns a value
+
+if __name__ == '__main__':
+    # Call the function in here if you want to test it
+    # Make sure it's indented
+    pass # remove or comment out this line if you wish to test the function
